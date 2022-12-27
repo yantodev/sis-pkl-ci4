ccb512abd5f780134b27271acfaf3ad622dc56d4
yanto <ekocahyanto007@gmail.com>
2022-12-27 15:07:52 +0700

    fixing address in rekap

--- app/Models/MasterDataModel.php
+++ app/Models/MasterDataModel.php
@@ -65,24 +65,24 @@ class MasterDataModel extends Model
     {
         if ($tp && $major) {
             $response = $this->db->table('master_data md')
-                ->select(
-                    'md . id, md . status, ud . name, i . id as idukaId, i . name as idukaName,
-                ud . user_id as nis,ud . jk, class.name as kelas, m . name as majorName,
-                di . address, tp . name as tpName, teacher . name as teacherName, teacher . hp, surat . detail_tgl')
-                ->join('tp', 'tp . id = md . tp_id')
-                ->join('iduka i', 'i . id = md . iduka_id')
-                ->join('detail_iduka di', 'di . id_iduka = i . id', 'left')
-                ->join('major m', 'm . id = i . major')
-                ->join('user_details ud', 'ud . user_public_id = md . user_public_id')
-                ->join('class', 'class.id = ud . class_id', 'left')
-                ->join('tutor', 'tutor . iduka_id = i . id', 'left')
-                ->join('teacher', 'teacher . user_public_id = tutor . teacher_id', 'left')
-                ->join('tbl_surat surat', 'surat . id_tp = md . tp_id', 'left')
-                ->where('tutor . deleted_at', null)
-                ->where('md . tp_id', $tp)
-                ->where('m . id', $major)
-                ->orderBy('ud . user_id', 'ASC')
-                ->orderBy('i . name', 'ASC')
+                ->select('
+                            md.id, md.status, ud.name, i.id as idukaId, i.name as idukaName,
+                            ud.user_id as nis, ud.jk, class.name as kelas, m.name as majorName,
+                            di.address, tp.name as tpName, teacher.name as teacherName, teacher.hp, surat.detail_tgl')
+                ->join('tp', 'tp.id = md.tp_id')
+                ->join('iduka i', 'i.id = md.iduka_id')
+                ->join('detail_iduka di', 'di.id_iduka = i.id', 'left')
+                ->join('major m', 'm.id = i.major')
+                ->join('user_details ud', 'ud.user_public_id = md.user_public_id')
+                ->join('class', 'class.id = ud.class_id', 'left')
+                ->join('tutor', 'tutor.iduka_id = i.id', 'left')
+                ->join('teacher', 'teacher.user_public_id = tutor.teacher_id', 'left')
+                ->join('tbl_surat surat', 'surat.id_tp = md.tp_id', 'left')
+                ->where('tutor.deleted_at', null)
+                ->where('md.tp_id', $tp)
+                ->where('m.id', $major)
+                ->orderBy('ud.user_id', 'ASC')
+                ->orderBy('i.name', 'ASC')
                 ->get()->getResult();
         } else {
             $response = $this->db->query('
@@ -99,7 +99,7 @@ class MasterDataModel extends Model
                     from master_data md
                              inner join tp t on md.tp_id = t.id
                              inner join iduka i on md.iduka_id = i.id
-                             left join detail_iduka di on md.iduka_id = di.id
+                             left join detail_iduka di on md.iduka_id = di.id_iduka
                              inner join major m on i.major = m.id
                              inner join user_details ud on md.user_public_id = ud.user_public_id
                              left join class c on ud.class_id = c.id
